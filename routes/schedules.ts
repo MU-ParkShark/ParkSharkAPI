3cf8142e61882e15353a4e90af1f477d0f0034a1
Davian Albarran <dalbarran.dev@gmail.com>
2023-12-14 22:45:49 -0500

    setting schedule is working

--- routes/schedules.ts
+++ routes/schedules.ts
@@ -1,7 +1,10 @@
 import express, { Router } from "express";
 import { Schedule } from "../models/Schedule";
+import bodyParser from "body-parser";
 
 export const schedulesRouter: Router = express.Router();
+
+const jsonParser = bodyParser.json();
 
 schedulesRouter.get('/', (_req, res) => {
     res.send('Schedules endpoint hit.');
@@ -14,9 +17,33 @@ schedulesRouter.get('/getUserSchedule/:id', async (req, res) => {
                 'user_id': parseInt(req.params.id)
             }
         })
-        res.send(resSchedule)
+        res.send(resSchedule);
     } catch(err) {
         console.log(err);
         res.status(404).send(err);
     }
-})
+});
+
+// Expects request body to be JSON data
+schedulesRouter.post('/setSchedule', jsonParser, async (req, res) => {
+    const scheduleData = {
+        user_id: req.body.userId || -1,
+        time_in: req.body.timeIn || null,
+        time_out: req.body.timeOut || null,
+        day_of_week: req.body.dayOfWeek || -1
+    };
+
+    // Data validation
+    if ( scheduleData.day_of_week == -1  || scheduleData.user_id == -1 || 
+           !scheduleData.time_in  || !scheduleData.time_out ) {
+        throw new Error('Not all data present! Request failed!');
+    }
+
+    try {
+        const newScheduleEntry = await Schedule.create(scheduleData);
+        res.status(200).send(newScheduleEntry);
+    } catch(err) {
+        console.log(err);
+        res.status(501).send(err);
+    }
+});
