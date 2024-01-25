0727680af09cf571caac1a714860e1ebb0673455
Davian A. Albarran <kostonkiddo10@gmail.com>
2024-01-25 17:03:31 -0500

    added the schemas i have at this point to their respective route files.

--- routes/schedules.ts
+++ routes/schedules.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { Schedule } from "../models/Schedule";
 import bodyParser from "body-parser";
+import { scheduleSchema } from "../models/JoiSchemas";
 
 export const schedulesRouter: Router = express.Router();
 
@@ -33,13 +34,8 @@ schedulesRouter.post('/setSchedule', jsonParser, async (req, res) => {
         day_of_week: req.body.dayOfWeek || -1
     };
 
-    // Data validation
-    if ( scheduleData.day_of_week == -1  || scheduleData.user_id == -1 || 
-           !scheduleData.time_in  || !scheduleData.time_out ) {
-        throw new Error('Not all data present! Request failed!');
-    }
-
     try {
+        await scheduleSchema.validateAsync(scheduleData);
         const newScheduleEntry = await Schedule.create(scheduleData);
         res.status(200).send(newScheduleEntry);
     } catch(err) {
