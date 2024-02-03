2d05d069631953d682a03a470fdbc7a0bb956d94
Davian Albarran <dalbarran.dev@gmail.com>
2024-02-02 22:05:53 -0500

    I have a plan for how i am gonna tackle this, not quite sure if it will work though

---
+++ Enums.ts
@@ -0,0 +1,5 @@
+export enum State {
+    PARKED = "PARKED",
+    TIMEOUT = "TIMEOUT",
+    UNDECIDED = "UNDECIDED"
+}
--- models/Parkinator.ts
+++ models/Parkinator.ts
@@ -1,10 +1,34 @@
 import { Sequelize } from "sequelize";
-import { dbConnection } from "./dbInit"
+import { Parking_Spot } from '../models/Parking_Spot';
+import { Lot_Activity } from "./Lot_Activity";
+import { State } from "../Enums";
+
+interface ILocation {
+    longitude: number,
+    latitude: number
+}
+
+interface IDriverCache {
+    tag_id: number,
+    location: ILocation,
+    locationUnchangedCount: number // Count how many times the location remains unchanged.
+}
 
 export default class Parkinator {
-    private dbConnection: Sequelize;
+    private static lastFiveLocations: Array<IDriverCache> = [];
 
-    constructor() {
-        this.dbConnection = dbConnection;
+    constructor() {}
+
+    private getNearbySpot(longitude: number, latitude: number) {
+        // TODO: Port Tyler's geoquery code here
+    }
+
+    private updateSpot(spot_id: number) {
+        // TODO: Update Parking_Spots table isAvailable and Lot_Activity pTimeIn, pTimeOut, timeslot, and day_of_week.
+    }
+
+    public determineState(tag_id: number, longitude: number, latitude: number) {
+        // TODO: utitlize above methods to determine if spot is taken and what state car is in.
+
     }
 }
