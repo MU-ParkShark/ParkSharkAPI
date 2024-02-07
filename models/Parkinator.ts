0bc9a4af6dc531ea963b6e0ad7b40b4363951c1d
Davian Albarran <dalbarran.dev@gmail.com>
2024-02-06 21:50:17 -0500

    db changed, gonna merge tyler's commits and finish this class

--- models/Parkinator.ts
+++ models/Parkinator.ts
@@ -8,27 +8,24 @@ interface ILocation {
     latitude: number
 }
 
-interface IDriverCache {
-    tag_id: number,
-    location: ILocation,
-    locationUnchangedCount: number // Count how many times the location remains unchanged.
-}
-
 export default class Parkinator {
-    private static lastFiveLocations: Array<IDriverCache> = [];
-
     constructor() {}
 
-    private getNearbySpot(longitude: number, latitude: number) {
+    private static getNearbySpot(longitude: number, latitude: number) {
         // TODO: Port Tyler's geoquery code here
     }
 
-    private updateSpot(spot_id: number) {
+    private static updateSpot(spot_id: number) {
         // TODO: Update Parking_Spots table isAvailable and Lot_Activity pTimeIn, pTimeOut, timeslot, and day_of_week.
+    }
+
+    private static updateCache(tag_id: string, location: ILocation, locationUnchangedCount: number) {
+        
     }
 
     public determineState(tag_id: number, longitude: number, latitude: number) {
         // TODO: utitlize above methods to determine if spot is taken and what state car is in.
-
+        let tag_id_string = `${tag_id}`;
+        const location: ILocation = { longitude, latitude };
     }
 }
